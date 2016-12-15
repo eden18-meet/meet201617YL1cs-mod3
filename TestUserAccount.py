597837c599621156cd66bd771301e6c54ec74b67
eden18-meet <edenv200@gmail.com>
2016-12-15 19:26:29 +0200

    my comment

--- TestUserAccount.py
+++ TestUserAccount.py
@@ -2,17 +2,16 @@
 from UserAccount import UserAccount
 
 #Three things are missing from the line below - fill them in
-my_user=UserAccount(self,password,secret)
-self.password=password
-self.secret=secret
+my_user=UserAccount("eden18-meet","1234","dog")
 
-def print_secret(self,password_attempd):
-    if 
+
+
     
 #Call the print_secret method (function) - it takes one input - a guess for the password.
-
 #Use the wrong password as input here
-my_user.
+my_user.print_secret("ffg")
 
-#Use the right password here
-my_user.
+
+##Use the right password here
+##my_user.print_secret("1234")
+
