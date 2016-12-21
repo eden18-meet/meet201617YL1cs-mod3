4673ecb2363defad394c91cd883d1c9610cf82dd
eden18-meet <edenv200@gmail.com>
2016-12-21 19:46:58 +0200

    make a class to describe a student

--- student.py
+++ student.py
@@ -1,5 +1,5 @@
-class student:
-    def _init_(self,name,hometown,age,height,favorite_ice_cream):
+class Student:
+    def __init__(self,name,hometown,age,height,favorite_ice_cream):
         self.name=name
         self.hometown=hometown
         self.age=age
@@ -13,9 +13,9 @@ class student:
         print(self.height)
         print(self.favorite_ice_cream)
 
-    def get_giraffe_gap():
-        get_giraffe_gap==500
-        return(get_giraffe_gap-self.height)
+    def get_giraffe_gap(self):
+        giraffe=500
+        return(giraffe-self.height)
     
         
         
---
+++ test_student.py
@@ -0,0 +1,5 @@
+from student import Student
+my_student=Student("may","hifa","15",160,"vanilla cookies")
+my_student.print_summary()
+print (my_student.get_giraffe_gap())
+
